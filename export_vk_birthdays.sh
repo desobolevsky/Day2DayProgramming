8974f11e65fcc1c4ce31b3f8bcbd156a279494d8
Denis Sobolevsky <desobolevsky@gmail.com>
2022-07-02 16:50:53 +0300

    Update export_vk_birthdays.sh

--- export_vk_birthdays.sh
+++ export_vk_birthdays.sh
@@ -3,7 +3,7 @@ export API_TOKEN=YOUR_TOKEN_HERE
 # Get birthdays of your friends in VK and export it in json file
 # in {'name': 'first_name last_name', 'bdate': 'bdate'} format  
 curl -XGET "https://api.vk.com/method/friends.get?v=5.131&access_token=${API_TOKEN}&fields=bdate" \
-    | jq '[.response.items[] | {name: (.first_name + " " + .last_name), bdate: .bdate}]' > birthday.json
+    | jq '[.response.items[] | {name: (.first_name + " " + .last_name), bdate: .bdate}]' > birthdays.json
 
 # Some of friends might have not specified their birthdays, so they are null in json file
 # You can separate friends with specified and not specified birthdays to different files
